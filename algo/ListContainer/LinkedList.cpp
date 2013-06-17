6b813d6d32983dd3d3ea887741a88c3d2233aebb
AKindyakov <akindyakov@gmail.com>
2013-06-17 13:21:38 +0400

    carry out list template class implementation to header

--- algo/ListContainer/LinkedList.cpp
+++ algo/ListContainer/LinkedList.cpp
@@ -1,16 +1,2 @@
 #include "ListContainer/LinkedList.h"
 
-template<class T>
-T* LinkedList<T>::begin()
-{
-    return m_start; 
-}
-
-template<class T>
-T* LinkedList<T>::end()
-{
-    return m_end; 
-}
-
-template<class T>
-LinkedListNode<T>* LinkedList<T>::m_end = new LinkedListNode<T>();
--- algo/ListContainer/LinkedList.h
+++ algo/ListContainer/LinkedList.h
@@ -1,52 +1,59 @@
+#include <cstdlib>
 
-template<class Item> 
-class LinkedListNode 
+class IListNode 
 {
 public:
-    LinkedListNode(){};
-    ~LinkedListNode(){};
-    
-    LinkedListNode* getNext(){ 
-        return new LinkedListNode(); 
-    };
-
-    
-private:
-    LinkedListNode* p_next;
-    Item* p_item;
+    IListNode(){};
+    virtual ~IListNode(){};
 };
 
+
 template<class Item> 
-class DublyLinkedListNode 
+class LinkedListNode : public IListNode 
 {
 public:
-    DublyLinkedListNode(){};
-    ~DublyLinkedListNode(){};
+    LinkedListNode();
+    LinkedListNode(Item*, LinkedListNode<Item>*);
+    virtual ~LinkedListNode();
     
-    DublyLinkedListNode* getNext() {
-        return new DublyLinkedListNode();
-    };
-
-    
-private:
-    DublyLinkedListNode* p_next;
-    Item* p_item;
+    LinkedListNode* next;
+    Item* item;
 };
 
 template<class T>
 class LinkedList
 {
 public:
-    LinkedList(){};
-    ~LinkedList(){};
+    LinkedList();
+    LinkedList(int size);
+    virtual ~LinkedList();
+   
+    void push_front(T); 
+    void push_front(T*); 
+    void pop_front(); 
     
-    T* begin(); 
-    T* end();
+    void erase(LinkedListNode<T>* begin, LinkedListNode<T>* end); 
+    
+    T* getBegin()
+    {
+        return start; 
+    }; 
     
 private:
-    LinkedListNode<T>* m_start;
-    static LinkedListNode<T>* m_end;
+    LinkedListNode<T>* start;
 };
+
+template<class Item> 
+class DublyLinkedListNode : public IListNode 
+{
+public:
+    DublyLinkedListNode(){};
+    ~DublyLinkedListNode(){};
+    
+    DublyLinkedListNode* next;
+    Item* item;
+};
+
 
 template<class T>
 class DublyLinkedList
@@ -55,8 +62,70 @@ public:
     DublyLinkedList(){};
     ~DublyLinkedList(){};
     
+    DublyLinkedListNode<T>* getNode(); 
 private:
-    LinkedListNode<T>* start;
-    LinkedListNode<T>* end;
+    DublyLinkedListNode<T>* start;
+    DublyLinkedListNode<T>* end;
 };
+
+template<class Item> 
+LinkedListNode<Item>::LinkedListNode()
+    : item(NULL), next(NULL) 
+{}
+
+template<class Item> 
+LinkedListNode<Item>::LinkedListNode(Item* _pItem, LinkedListNode<Item>* _pNode)
+    : item(_pItem), next(_pNode) 
+{}
+
+template<class Item> 
+LinkedListNode<Item>::~LinkedListNode()
+{
+    
+}
+
+template<class T>
+LinkedList<T>::LinkedList()
+    : start(new LinkedListNode<T>())
+{}
+
+template<class T>
+LinkedList<T>::LinkedList(int n)
+    : start(new LinkedListNode<T>())
+{
+    for (int i=0; i<n; ++i)
+    {
+        push_front(new T()); 
+    }
+}
+
+template<class T>
+void LinkedList<T>::push_front(T elem)
+{
+    start = new LinkedListNode<T>(new T(elem), start);
+}
+
+template<class T>
+void LinkedList<T>::push_front(T* elem)
+{
+    start = new LinkedListNode<T>(elem, start);
+}
+
+template<class T>
+void LinkedList<T>::pop_front()
+{
+    LinkedListNode<T>* erasingElem = start;
+    start = start->next;
+    delete erasingElem->item; 
+    delete erasingElem; 
+}
+
+template<class T>
+LinkedList<T>::~LinkedList()
+{
+    while (start->next != NULL)
+    {
+        this->pop_front();
+    }
+}
 
