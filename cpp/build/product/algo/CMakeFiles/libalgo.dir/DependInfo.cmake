93ff5e9365e184eab0a99b16c9813f0fd3fb4b3e
AKindyakov <akindyakov@gmail.com>
2013-09-13 00:46:53 +0400

    restructurisation

---
+++ cpp/build/product/algo/CMakeFiles/libalgo.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alexander/source/simple_algo/algo/ListContainer/LinkedList.cpp" "/home/alexander/source/simple_algo/build/product/algo/CMakeFiles/libalgo.dir/ListContainer/LinkedList.cpp.o"
+  "/home/alexander/source/simple_algo/algo/ListContainer/dublyLinkedList.cpp" "/home/alexander/source/simple_algo/build/product/algo/CMakeFiles/libalgo.dir/ListContainer/dublyLinkedList.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../algo"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ cpp/build/product/algo/CMakeFiles/libalgo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/libalgo.dir/ListContainer/LinkedList.cpp.o"
+  "CMakeFiles/libalgo.dir/ListContainer/dublyLinkedList.cpp.o"
+  "liblibalgo.pdb"
+  "liblibalgo.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/libalgo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
