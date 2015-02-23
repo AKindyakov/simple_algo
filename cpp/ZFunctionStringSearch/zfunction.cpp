05493d86f71bdc6c01a4ecfecf517e229c6d1c1a
akindyakov <akindyakov@gmail.com>
2015-02-24 01:38:55 +0300

    cut off debug stderr out

--- cpp/ZFunctionStringSearch/zfunction.cpp
+++ cpp/ZFunctionStringSearch/zfunction.cpp
@@ -1,12 +1,17 @@
 #include "zfunction.h"
 
+/*
+* [ccccccccccccccc]
+*      ^    ^
+*      s    f
+* return number of first mismatch 's'
+*/
 static inline size_t
 findFirstMissmatch(
     const std::string str,
     size_t s,
     size_t f
 ) {
-    //*debug*/ std::cerr << " find( " << s << ", " << f << " ): ";
     size_t len = str.size();
     while (f < len) {
         if (str[s] != str[f]) {
@@ -15,7 +20,6 @@ findFirstMissmatch(
         ++f;
         ++s;
     }
-    //*debug*/ std::cerr << f << " ";
     return f;
 }
 
@@ -35,33 +39,22 @@ zFunction(
     }
 
     z[1] = findFirstMissmatch(str, 0, 1) - 1;
-    //*debug*/ std::cerr << std::endl;
-
     for (size_t s = 1, f = 2; f < len; ++f) {
         size_t ff = f - s;
-        //*debug*/ std::cerr
-        //*debug*/     << "s ["  << s  << "]: '" << str[s] << "' "
-        //*debug*/     << "f ["  << f  << "]: '" << str[f] << "' "
-        //*debug*/ ;
-        //*debug*/ std::cerr << " ff:" << ff << " + z[ff]: " << z[ff] << " > " << z[s];
         size_t zf = 0;
         if (s + z[s] <= f) { // bad base point - full scan
-            //*debug*/ std::cerr << " {1.1} ";
             zf = findFirstMissmatch(str, 0, f) - f;
             s = f;
         } else if (ff + z[ff] > z[s]) {
-            //*debug*/ std::cerr << " {1.2} ";
             zf = findFirstMissmatch(str, z[ff], s + z[s]) - f;
             s = f;
         } else {
-            //*debug*/ std::cerr << " {2} ";
             zf = z[ff];
         }
         if (f + zf > len) {
             zf = len - f;
         }
         z[f] = zf;
-        //*debug*/ std::cerr << "z[ " << f << " ] = " << z[f] << std::endl;
     }
     return z;
 }
--- cpp/ZFunctionStringSearch/zfunction.h
+++ cpp/ZFunctionStringSearch/zfunction.h
@@ -4,13 +4,6 @@
 #include <vector>
 #include <iostream>
 
-/*
-* [ccccccccccccccc]
-*      ^    ^
-*      s    f
-* return number of first mismatch 's'
-*/
-
 std::vector<size_t>
 zFunction(
     const std::string& str
