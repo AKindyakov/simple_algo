5567d2e761236a30fdc5c57e69438d24cb5dc983
akindyakov <akindyakov@gmail.com>
2015-03-29 01:20:37 +0300

    Fix interface for tree

--- cpp/SuffuxTree/tree.h
+++ cpp/SuffuxTree/tree.h
@@ -1,70 +1,63 @@
+#pragma once
 #include <iostream>
 #include <string>
+#include <memory>
+#include <vector>
 
 namespace NSufixTree {
 
 const size_t ABC_SIZE = 26;
 const char FIRST_ABC_CHAR = 'a';
 
-class TNode;
-
 struct TSubstring {
-    TSubstring(const std::string& base, size_t s = 0, size_t e = std::string::npos)
-        : start(s), end(e)
+    TSubstring(
+        const std::string* _str,
+        size_t _start = 0,
+        size_t _end = std::string::npos
+    )
+        : str(_str)
+        , start(_start)
+        , end(_end)
     {
     }
+
+    const std::string* str;
     size_t start;
     size_t end;
-    const std::string* str;
 };
+
+class TNode;
 
 class TEdge {
 public:
-    TEdge()
-        :   from(NULL), to(NULL),
-            prevGuest(0) {
-        sub.start = -1;
-        sub.end = -1;
+    TEdge(
+        const TSubstring& sub,
+        TNode* parent
+    )
+        : parentNode(parent)
+        , subString(sub)
+    {
     }
 
-    TEdge( TNode* _from, TNode* _to, int start, int end, int count=0 )
-        :   from(_from), to(_to),
-            prevGuest(count) {
-        sub.start = start;
-        sub.end = end;
+    char head() const {
+        return subString.str->at(subString.start);
     }
 
-    char head() const;
     void showMe(size_t lvl, std::ostream& os) const;
-    size_t match(const string& modelStr, size_t start, size_t end) const;
+    size_t match(const TSubstring& sub) const;
 
-    void finish(const string& str);
-
-private:
-    TNode* parentNode;
+    TNode* parentNode = nullptr;
     std::unique_ptr<TNode> endNode;
-    TSubstring sub;
+    TSubstring subString;
 };
 
 class TNode {
 public:
-    TNode();
+    void showMe(size_t lvl, std::ostream& os) const;
 
-    void addEdge( char ch, TNode* to, int start, int end, int strCount );
-
-    TEdge* findEdge( char ch );
-
-    TEdge* firstEdgePt() {
-        return *edges;
-    }
-
-    int  greatestSubstring( int lvl, list<TSubstring>* outSub );
-    int findSub( const string& modelStr, const string& str, int start, int end )const;
-    void finish(const string& str);
-    void showMe(const string& str, int lvl, std::ostream& os) const;
-
-    TEdge* edges[ABC_SIZE];
-    TEdge* parrent;
+    std::array<std::unique_ptr<TEdge>, ABC_SIZE> edges;
+    TEdge* parrent = nullptr;
+    TNode* suffixLink = nullptr;
 };
 
 struct SuffTreeCursor {
@@ -75,18 +68,26 @@ struct SuffTreeCursor {
     int cursor;
 };
 
-class TSuffixTree {
+class TTreeBase {
 public:
-    TSuffixTree(const std::string& str);
-    virtual ~TSuffixTree();
+    TTreeBase(const std::string& str)
+        : text(str)
+    {
+        ukkonenRebuildTree();
+    }
 
-    std::vector<size_t> match(const std::string& pattern) const;
+    virtual ~TTreeBase() {}
+
     void show(std::ostream& os) const;
 
 private:
+    void ukkonenRebuildTree();
+
     TNode root;
     std::string text;
 };
 
 }
+
+using TSuffixTree = NSufixTree::TTreeBase;
 
--- cpp/SuffuxTree/ut_test.cpp
+++ cpp/SuffuxTree/ut_test.cpp
@@ -1,77 +1,29 @@
-#include <iostream>
-#include <exception>
 #include "tree.h"
-#include <vector>
 
 #include "../SimpleAlgoUtil/simple_exception.h"
+#include "../SimpleAlgoUtil/unit_test.h"
 
-int main( int argN, char** argS ) {
+#include <exception>
+#include <iostream>
+#include <vector>
+
+int main(int /*argn*/, const char** /*args*/) {
     try {
-        int samples = 0;
-        std::cin >> samples;
-        std::cin.ignore();
-        if( samples < 1 || samples > 10) {
-            throw TSimpleException("Unexpected algo list");
-        }
-
-        //std::vector<SuffTree> tree(--samples);
-        SuffTree tree[9];
-        for( int i=0; i < samples-1; ++i ) {
-            char ch = std::cin.get();
-            while( ch != '\n' ) {
-                tree[i].add(ch);
-                ch = std::cin.get();
-                //**/ tree[samples].showMe(std::cout);
-            }
-            tree[i].finishTree();
-            //**/ std::cout << "\n\n---------------------\n";
-            //**/ tree[i].showMe(std::cout);
-        }
-        //**/ std::cout << "Generate compleete\n";
-
-        std::string lastStr;
-        char ch = std::cin.get();
-        while( ch != '\n' ) {
-            lastStr.push_back(ch);
-            ch = std::cin.get();
-        }
-        size_t slen = lastStr.size();
-        int rstart=0, rend=slen;
-        int maxLen = 0;
-        for( size_t st=0; st < slen; ++st ) {
-            //std::cout << "st: " << st << '\n';
-            if( slen-st < maxLen ) {
-                //**/ std::cout << "break: " << 1 << '\n';
-                break;
-            }
-            int wrk_end=slen;
-            for( int i=0; i<samples-1; ++i ) {
-                wrk_end = tree[i].findSub(lastStr, st, wrk_end);
-                //**/ std::cout << "st: " << st << " i: " << i
-                //**/           << " [" << st << ' ' << wrk_end << "]\n";
-                if( wrk_end - st <= maxLen ) {
-                    //**/ std::cout << "break: " << 2 << '\n';
-                    break;
-                }
-            }
-            if( wrk_end - st > maxLen ) {
-                maxLen = wrk_end - st;
-                rstart = st;
-                rend   = wrk_end;
-                //**/ std::cout << "max: " << maxLen
-                //**/           << " [" << rstart << ' ' << rend << "]\n";
-            }
-        }
-        //**/ tree.showMe(std::cout);
-        std::cout << lastStr.substr(rstart, rend-rstart) << '\n';
+        std::string str = "text";
+        TSuffixTree stree(str);
+    }
+    catch(const TSimpleException& e) {
+        std::cerr
+            << "TSimpleException error "
+            << e.what() << '\n';
     }
     catch(const std::exception& e) {
-        std::cerr << "Std error "
-                  << e.what() << '\n';
+        std::cerr
+            << "Std error "
+            << e.what() << '\n';
     }
     catch(...) {
-        std::cerr << "Samething wrong\n";
+        std::cerr << "Unexpected error";
     }
-
     return 0;
 }
