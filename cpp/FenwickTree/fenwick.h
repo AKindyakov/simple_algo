0275035a23c351020750209563fb3e01ba43ff00
akindyakov <akindyakov@gmail.com>
2016-04-05 13:02:48 +0300

    Get and Recont realisation

--- cpp/FenwickTree/fenwick.h
+++ cpp/FenwickTree/fenwick.h
@@ -1,31 +1,159 @@
+#include "../SimpleAlgoUtil/simple_exception.h"
+
 #include <cstdlib>
 #include <vector>
-
+namespace NFenwickTree {
 template<
-    typename Type,
-    typename Direct,
-    typename Reverse
+    typename Type
+    , typename Add
+    , typename Sub
+    , Type Initial = 0
 >
 class TFenwickTree {
 public:
+    /**
+    *
+    */
     TFenwickTree();
+
+    /**
+    *
+    */
     explicit TFenwickTree(std::vector<Type>&& src);
+
+    /**
+    *
+    */
     explicit TFenwickTree(size_t length);
+
+    /**
+    *
+    */
     template<typename InputIt>
     TFenwickTree(InputIt first, InputIt last);
 
+    /**
+    *
+    */
     void Swap(size_t index, Type newValue);
+
+    /**
+    *
+    */
     void Push(Type value);
+
+    /**
+    *
+    */
     void Pop(Type value);
 
+    /**
+    * O(logN)
+    */
     Type Get(size_t start, size_t end) const;
 
 private:
-    inline size_t BitG(size_t index) {
+    static inline size_t BitG(size_t index) {
         return index & (index + 1);
     }
 
+    void Recount();
+    void UpdateOne(size_t pIndex);
 private:
     std::vector<Type> Array;
     std::vector<Type> Partial;
 };
+
+template<typename Type>
+Type Add(Type one, Type two) {
+    return one + two;
+}
+
+template<typename Type>
+Type Sub(Type from, Type what) {
+    return from - what;
+}
+
+template<
+    typename Type
+    , typename A
+    , typename S
+>
+TFenwickTree<Type, A, S>
+Create(std::vector<Type>&& arr, A add, S sub) {
+    return TFenwickTree<Type, A, S>(std::move(arr));
+}
+
+template<
+    typename Type
+    , typename Add
+    , typename Sub
+    , Type Initial
+>
+TFenwickTree<Type, Add, Sub, Initial>::TFenwickTree(std::vector<Type>&& arr)
+    : Array(std::move(arr))
+{
+    Recount();
+}
+
+template<
+    typename Type
+    , typename Add
+    , typename Sub
+    , Type Initial
+>
+void TFenwickTree<Type, Add, Sub, Initial>::Recount() {
+    for (size_t pi = 0; pi < Partial.size(); ++pi) {
+        UpdateOne(pi);
+    }
+}
+
+template<
+    typename Type
+    , typename Add
+    , typename Sub
+    , Type Initial
+>
+void TFenwickTree<Type, Add, Sub, Initial>::UpdateOne(size_t pIndex) {
+    size_t g = BitG(pIndex);
+    //Type& p = Partial[pIndex];
+    for (size_t gi = g; g <= pIndex; ++gi) {
+         Partial[pIndex] = Add(Partial[pIndex], Array[gi]);
+         //Partial[pIndex] = Partial[pIndex] + Array[gi];
+    }
+}
+
+template<
+    typename Type
+    , typename Add
+    , typename Sub
+    , Type Initial
+>
+Type TFenwickTree<Type, Add, Sub, Initial>::Get(size_t start, size_t end) const {
+    if (end == 0 || start == end) {
+        return 0;
+    }
+    if (start < end) {
+        throw TSimpleException("Wrong interval, start less then end")
+            << "(" << start << " < " << end << ")";
+    }
+    Type ret = Initial;
+    while (true) {
+        ret = Add(ret, Partial[end]);
+        end = BitG(end);
+        if (end == 0) {
+            break;
+        }
+        --end;
+    }
+    while (true) {
+        ret = Sub(ret, Partial[start]);
+        start = BitG(start);
+        if (start == 0) {
+            break;
+        }
+        --start;
+    }
+    return ret;
+}
+}
--- cpp/FenwickTree/ut_test.cpp
+++ cpp/FenwickTree/ut_test.cpp
@@ -1,7 +1,18 @@
 #include "../SimpleAlgoUtil/unit_test.h"
 
+#include "fenwick.h"
+namespace NFT = NFenwickTree;
+
 int EmptyFunction(int a, int b) {
     return a + b + 1;
+}
+
+void SumTest() {
+    auto tree = NFT::Create(
+        std::vector<int>{1, 2, 3, 4}
+        , NFT::Add<int>
+        , NFT::Sub<int>
+    );
 }
 
 int main() {
