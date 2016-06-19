0f042c841e6843b31d928f860c75bd8dac344a16
akindyakov <akindyakov@gmail.com>
2016-06-19 13:45:41 +0300

    mv eratosthensSieve.py eratosthens_sieve.py

--- python/PrimeNumber/eratosthens_sieve_ut.py
+++ python/PrimeNumber/eratosthens_sieve_ut.py
@@ -1,13 +1,13 @@
-import eratosthensSieve
+import eratosthens_sieve as es
 
 def test_get_all_prime_0():
-    assert list(eratosthensSieve.get_all_prime(0)) == []
+    assert list(es.get_all_prime(0)) == []
 
 def test_get_all_prime_1():
-    assert list(eratosthensSieve.get_all_prime(1)) == [1]
+    assert list(es.get_all_prime(1)) == [1]
 
 def test_get_all_prime_2():
-    assert list(eratosthensSieve.get_all_prime(2)) == [1, 2]
+    assert list(es.get_all_prime(2)) == [1, 2]
 
 def test_get_all_prime_4():
-    assert list(eratosthensSieve.get_all_prime(4)) == [1, 2, 3]
+    assert list(es.get_all_prime(4)) == [1, 2, 3]
